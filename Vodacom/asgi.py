2d0626cd5e94ef9a7e9860d6495b935d9e00fbe0
oscarogar <oscar.ogar@yahoo.com>
2025-06-23 12:22:25 +0100

    Clean start without history or secrets

---
+++ Vodacom/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Vodacom project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vodacom.settings')
+
+application = get_asgi_application()
