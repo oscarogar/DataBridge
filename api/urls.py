a4552ace74f3f57dc4fc4f3b6e3535e621aea80f
oscarogar <oscar.ogar@yahoo.com>
2025-06-26 11:08:40 +0100

    ytop 20

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,13 @@ urlpatterns = [
     path('sales-trend/', views.sales_trend_analytics, name='sales_trend'),
     path('profit-analysis/', views.profit_margin_analytics),
     path('cost-analysis/', views.cost_analysis, name='cost_analysis'),
-    path("python-version/", views.python_version_view),
     path("overview/", views.sales_summary),
+    
     path("transaction-summary/", views.transaction_summary),
     path("top-performing-businesses/", views.transaction_entities_analysis),
     path("timing-analysis/", views.transaction_timing_analysis),
+    
+    
     path("product-demand-analysis/", views.product_demand_analysis),
     path("product-revenues/", views.product_revenue_analysis),
     path("product-correlation/", views.product_correlation_analysis),
@@ -19,6 +21,14 @@ urlpatterns = [
     path("order-fulfillment-analysis/", views.order_fulfillment_analysis),
     path("order-calculation-analysis/", views.order_calculation_analysis),
     path("customer-segmentation-analysis/", views.customer_segmentation_analysis),
-    path('customer-buying-patterns/', views.customer_purchase_pattern_analysis)
+    path('customer-buying-patterns/', views.customer_purchase_pattern_analysis),
+    
+    
+    
+    
+    
+    
+    
+    path("python-version/", views.python_version_view),
     
 ]
